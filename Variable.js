d96a84312f61c5b9176e79698292d1c3261376f4
PratisthaChinara2023 <120037046+PratisthaChinara2023@users.noreply.github.com>
2025-08-25 06:25:04 +0000

    modified

--- Variable.js
+++ Variable.js
@@ -1,5 +1,5 @@
 const accountId=14554
 let email="Pratisthachinara@gmail.com"
-var Myname="Pratistha"
-console.log(accountId+" "+email+" "+Myname);
+var password="Pratistha"
+console.log(accountId+" "+email+" "+password);
 
