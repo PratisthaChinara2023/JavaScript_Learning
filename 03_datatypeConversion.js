7f1608b496d6a738ea57eb54b5206f543287e3ce
PratisthaChinara2023 <120037046+PratisthaChinara2023@users.noreply.github.com>
2025-08-25 15:54:54 +0000

    datatype Conversion

---
+++ 03_datatypeConversion.js
@@ -0,0 +1,16 @@
+// let score="33aa"
+// let num=Number(score)
+// console.log(num)//NAN
+// console.log(typeof num)// number
+
+//--------------------Operation -------------------//
+let value =3
+let neg=-value
+console.log(neg)
+
+let str1="hitesg"
+let str2="hiTESH"
+console.log(str1+str2)
+
+console.log(+true)// 1
+// console.log(true+)// error
