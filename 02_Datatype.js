5aa75edda0d1e01eab3d842e90454349b5409093
PratisthaChinara2023 <120037046+PratisthaChinara2023@users.noreply.github.com>
2025-08-25 10:25:06 +0000

    Datatype

---
+++ 02_Datatype.js
@@ -0,0 +1,16 @@
+"use strict"; // treat all js code as newer version
+//alert(3+3) this will give erroe as we are using node js, not browser(javascript engine)
+
+let name="Pratistha"
+let age=25
+let isLoggedIn=false
+// number to power 53
+// bigint
+// string=>""
+// boolean=> true/false
+// null=>standalone value
+//undefined=>
+// symbol=>unique
+// object
+console.log(typeof null)  // object
+console.log(typeof  undefined) //undefined
