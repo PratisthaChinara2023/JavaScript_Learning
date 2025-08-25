9f22d95c7383b2e7e425668bdcb05b55a92000f5
PratisthaChinara2023 <120037046+PratisthaChinara2023@users.noreply.github.com>
2025-08-25 06:35:12 +0000

    Completely done

--- Variable.js
+++ Variable.js
@@ -1,5 +1,10 @@
 const accountId=14554
+//accountId=222  not allowed because const can not be changed
 let email="Pratisthachinara@gmail.com"
 var password="Pratistha"
+accountCity="Jaipur"// can be declared but not a good practice
+// prefer not to use var because of the issue in block scope and functional scope
+let accountState; // output will be undefined
 console.log(accountId+" "+email+" "+password);
+console.table([accountId,email,password])
 
