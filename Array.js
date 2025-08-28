a7d2692d659c90552ecb3122fb4f4d55ddce29d1
PratisthaChinara2023 <120037046+PratisthaChinara2023@users.noreply.github.com>
2025-08-28 17:57:43 +0000

    Array

---
+++ Array.js
@@ -0,0 +1,10 @@
+ const arr=[0,1,2,3,4,5]
+// console.log(arr.push(2))
+// console.log(arr)
+// console.log(arr.pop())
+// console.log(arr.includes(5))// true
+console.log(arr)// type is array
+const arr1=arr.join()
+console.log(arr1)
+console.log(typeof arr1)// typoes has changed to string 
+
