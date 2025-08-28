8bc0dab2c24311ef160e5bb4d62e39842ef11afe
PratisthaChinara2023 <120037046+PratisthaChinara2023@users.noreply.github.com>
2025-08-28 17:03:59 +0000

    String

---
+++ String.js
@@ -0,0 +1,27 @@
+// const str="Pratistha"
+// const repocount=50
+// console.log(str+repocount)// concatination but outdated
+
+// console.log(`My name is ${str} and my repocount is ${repocount}`)// This is recomended
+// const name= new String('Pratsitha')
+// console.log(name[0])// h 
+// console.log(name.length)
+// console.log(name.toUpperCase())
+
+// console.log(name.charAt(2))//a
+// const newstr=name.substring(0,4)
+// console.log(newstr)//Prat
+
+// const newstr1=name.slice(-8,4)
+// console.log(newstr1)//rat
+
+
+const newstring="   Pratsiths      "
+console.log(newstring)
+console.log(newstring.trim())// remove all space
+
+const url="https//hitesh.com/hitest%20Choudhary"
+console.log(url.replace('%20','44')) //https//hitesh.com/hitest44Choudhary
+
+const s="prat--chin-2020"
+console.log(s.split('-'))// [ 'prat', 'chin', '2020' ]s
