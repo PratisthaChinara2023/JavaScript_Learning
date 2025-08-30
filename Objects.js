15150b23e4ea35d5cf378ba40e99f1679012eb72
PratisthaChinara2023 <120037046+PratisthaChinara2023@users.noreply.github.com>
2025-08-30 18:22:37 +0000

    Object 01

---
+++ Objects.js
@@ -0,0 +1,35 @@
+//Singleton
+// Object Literials
+
+
+//const mySymbol=Symbol("Key1")
+ const obj={
+    name:"Pratistha", 
+    "full name":"Prtaistga Chinara",
+    mySymbol:"mykey1",
+    //[mySymbol]:"mykey1",// [Symbol(Key1)] as output
+    age:25,
+    location:"jaipur",
+    email:"Ptati@gmail.com",
+    isloggedin:true
+ }
+//  console.log(obj.email)//Ptati@gmail.com
+//  console.log(obj["email"])//Ptati@gmail.com
+//  console.log(obj.full name)// Error1 that is why u should not use obj. becausse u cannot acces this type.
+// console.log(obj["full name"])// Prtaistga Chinara
+
+// console.log(obj.mySymbol)// not correct practice
+// console.log(typeof obj[mySymbol])// correct syntax 
+  //Use freeze to stop changes in the object
+// Object.freeze(obj)// The changes will not be made email=hitesh@gmail.com
+// obj.email="Hitesh@gmail.com"
+// console.log(obj)
+obj.greeting=function(){
+   console.log("hello js User");
+}
+console.log(obj.greeting())
+// Refer from object
+obj.greetingTwo=function(){
+   console.log(`hello ${this.name}`);// if you want to reference same object then use this.
+}
+console.log(obj.greetingTwo())
