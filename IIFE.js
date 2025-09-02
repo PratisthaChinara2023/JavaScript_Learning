f6562848c4a73b6c8826967f74571a221600e7fd
PratisthaChinara2023 <120037046+PratisthaChinara2023@users.noreply.github.com>
2025-09-02 11:29:17 +0000

    IIFE

---
+++ IIFE.js
@@ -0,0 +1,20 @@
+// Immediately invoked function expression (IIFE)
+/*Avoid global scope pollution → Variables inside an IIFE stay private and don’t interfere with variables in other scripts.
+
+Data privacy → It lets you create private variables and functions that cannot be accessed from outside.
+
+Immediate execution → Useful for running setup or initialization code right away.
+
+Encapsulation → Groups related logic together without exposing internal details.
+
+In short: IIFE is used to execute code immediately while keeping variables and logic private. */
+
+//Named IIFE
+ (function chai(){
+    console.log(`db connected`)
+
+ })();
+//UnNamed IIFE with oassing parameters
+((name)=>{
+    console.log(`db connected two ${name}`)
+})('Hiets')
