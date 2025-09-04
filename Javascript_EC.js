6d64631ca682a4e7ea4a8204dea0c95262fe27ce
PratisthaChinara2023 <120037046+PratisthaChinara2023@users.noreply.github.com>
2025-09-04 07:25:28 +0000

    Javascript Execution Context

---
+++ Javascript_EC.js
@@ -0,0 +1,10 @@
+let val1=10
+let val2=5
+function addValue(num1,num2)
+{
+    let total=num1+num2
+    return total
+}
+let result1=addValue(val1,val2)
+let result2=addValue(10,2)
+console.log(result1)
