2c89f4196eed8be869e89c03b9db3f3fa9f1fcc6
PratisthaChinara2023 <120037046+PratisthaChinara2023@users.noreply.github.com>
2025-09-05 10:54:23 +0000

    filter and  map

--- filter_map.js
+++ filter_map.js
@@ -13,3 +13,16 @@ mynum.forEach((num)=>{
     }
  })
  console.log(newarr)//[ 6, 7, 8, 9 ]
+
+ // Map
+
+ const mynumber=[1,2,3,4,5,6,7,8,9,10]
+ const sum=mynumber.map((num)=>num+10)
+ console.log(sum)
+
+ // chaining
+ const newNum=mynumber
+                    .map((num)=>num*10)
+                    .map((num)=>num+1)
+                    .filter((num)=>num>=40)
+console.log(newNum)
